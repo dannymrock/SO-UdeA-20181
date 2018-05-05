f704d5c53bfd6590bf88552b3afe800f06e719a0
tigarto <helpu1409@gmail.com>
2018-05-05 11:31:41 -0500

    actuzalicion del ejemplo

--- Lab4/ejemplos_guia4/parte1/ejemplos_de_refuerzo/simple_threads.c
+++ Lab4/ejemplos_guia4/parte1/ejemplos_de_refuerzo/simple_threads.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+#include <pthread.h>
+
 void do_one_thing(int *);
 void do_another_thing(int *);
 void do_wrap_up(int, int);
 
 int r1 = 0, r2 = 0;
 
-extern int
-main(void)
+extern int main(void)
 {
-  do_one_thing(&r1);
-  do_another_thing(&r2);
+  pthread_t       thread1,  thread2;
+
+  pthread_create(&thread1,
+          NULL,
+          (void *) do_one_thing,
+          (void *) &r1);
+
+  pthread_create(&thread2,
+          NULL,
+          (void *) do_another_thing,
+          (void *) &r2);
+
+  pthread_join(thread1,  NULL);
+  pthread_join(thread2,  NULL);
+
   do_wrap_up(r1, r2);
   return 0;
 }
